c589e65c28cf934d419fb96c63b4d3c800e0d775
Nishul Juneja <nishuljuneja@gmail.com>
2026-03-06 14:24:32 -0800

    Increase mismatched card display time to 2 seconds

--- src/app/games/word-match/page.tsx
+++ src/app/games/word-match/page.tsx
@@ -292,7 +292,7 @@ export default function WordMatchPage() {
           );
           setFlippedIds([]);
           setLockBoard(false);
-        }, 800);
+        }, 2000);
       }
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
